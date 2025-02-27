94492b61bb45a2e0933b66f2aa372854b871a40c
Paul De Silos <pauldesilos@yahoo.com>
2025-02-27 14:37:03 -0600

    updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
-import { Stack } from 'expo-router';
+import { Stack } from "expo-router";
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return <Stack initialRouteName="loading" screenOptions={{ headerShown: false }} />;
 }
